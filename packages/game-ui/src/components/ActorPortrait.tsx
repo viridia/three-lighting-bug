51fa9677c30551bce0ef7dc01b4f7ff147084d26
Talin <viridia@gmail.com>
2023-05-06 19:49:26 -0700

    Removed reference to deleted image.

--- packages/game-ui/src/components/ActorPortrait.tsx
+++ packages/game-ui/src/components/ActorPortrait.tsx
@@ -1,4 +1,3 @@
-import haloGreen from '../images/hud/halo-green.svg?url';
 import * as styles from './ActorPortrait.css';
 import { VoidComponent } from 'solid-js';
 import { ActorCloseUp } from './ActorCloseUp';
@@ -7,7 +6,6 @@ export const ActorPortrait: VoidComponent<{ side?: 'left' | 'right' }> = props =
   return (
     <div class={styles.layout}>
       <div class={styles.portraitElt}>
-        <img class={styles.haloImg} src={haloGreen} />
         <ActorCloseUp class={styles.portraitCanvas} side={props.side} />
       </div>
     </div>
